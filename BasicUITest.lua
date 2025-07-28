54081332d885a4a1f6d30d7710b6e36e76118263
IAmVeryNewToThisWebsite <fofoangel858@gmail.com>
2025-07-28 15:45:41 -0600

    Update BasicUITest.lua

--- BasicUITest.lua
+++ BasicUITest.lua
@@ -15,35 +15,35 @@ local Other = Instance.new("ImageButton")
 local Topbar = Instance.new("Frame")
 local UICorner_3 = Instance.new("UICorner")
 local TextLabel = Instance.new("TextLabel")
-local Tab_Universal = Instance.new("Frame")
+local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+local TextLabel_2 = Instance.new("TextLabel")
 local UICorner_4 = Instance.new("UICorner")
-local Tabs_2 = Instance.new("Frame")
+local Tab_Universal = Instance.new("Frame")
 local UICorner_5 = Instance.new("UICorner")
+local Tabs_2 = Instance.new("Frame")
+local UICorner_6 = Instance.new("UICorner")
 local Universal_2 = Instance.new("ImageButton")
 local Home_2 = Instance.new("ImageButton")
 local Other_2 = Instance.new("ImageButton")
 local Topbar_2 = Instance.new("Frame")
-local UICorner_6 = Instance.new("UICorner")
-local TextLabel_2 = Instance.new("TextLabel")
-local _64Speed = Instance.new("TextButton")
 local UICorner_7 = Instance.new("UICorner")
-local Tab_Other = Instance.new("Frame")
+local TextLabel_3 = Instance.new("TextLabel")
+local _64Speed = Instance.new("TextButton")
 local UICorner_8 = Instance.new("UICorner")
-local Tabs_3 = Instance.new("Frame")
+local Tab_Other = Instance.new("Frame")
 local UICorner_9 = Instance.new("UICorner")
+local Tabs_3 = Instance.new("Frame")
+local UICorner_10 = Instance.new("UICorner")
 local Universal_3 = Instance.new("ImageButton")
 local Home_3 = Instance.new("ImageButton")
 local Other_3 = Instance.new("ImageButton")
 local Topbar_3 = Instance.new("Frame")
-local UICorner_10 = Instance.new("UICorner")
-local TextLabel_3 = Instance.new("TextLabel")
-local LearnLua = Instance.new("TextButton")
 local UICorner_11 = Instance.new("UICorner")
-local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
-local HideShow = Instance.new("TextButton")
+local TextLabel_4 = Instance.new("TextLabel")
+local LearnLua = Instance.new("TextButton")
 local UICorner_12 = Instance.new("UICorner")
-local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
-local UIAspectRatioConstraint_3 = Instance.new("UIAspectRatioConstraint")
+local HideShow = Instance.new("TextButton")
+local UICorner_13 = Instance.new("UICorner")
 
 --Properties:
 
@@ -53,11 +53,12 @@ BasicUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 Ui.Name = "Ui"
 Ui.Parent = BasicUI
+Ui.AnchorPoint = Vector2.new(0.5, 0.5)
 Ui.BackgroundColor3 = Color3.fromRGB(126, 159, 177)
 Ui.BackgroundTransparency = 0.100
 Ui.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Ui.BorderSizePixel = 0
-Ui.Position = UDim2.new(0.344019443, 0, 0.246121183, 0)
+Ui.Position = UDim2.new(0.515740454, 0, 0.499425143, 0)
 Ui.Size = UDim2.new(0, 397, 0, 266)
 
 Tab_Home.Name = "Tab_Home"
@@ -67,7 +68,6 @@ Tab_Home.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Tab_Home.BorderSizePixel = 0
 Tab_Home.Position = UDim2.new(0.151133507, 0, 0.157894731, 0)
 Tab_Home.Size = UDim2.new(0, 329, 0, 217)
-Tab_Home.Visible = false
 
 UICorner.Parent = Tab_Home
 
@@ -137,6 +137,24 @@ TextLabel.TextColor3 = Color3.fromRGB(192, 192, 192)
 TextLabel.TextSize = 25.000
 TextLabel.TextXAlignment = Enum.TextXAlignment.Left
 
+UIAspectRatioConstraint.Parent = Tab_Home
+UIAspectRatioConstraint.AspectRatio = 1.516
+
+TextLabel_2.Parent = Tab_Home
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(0.0334345587, 0, 0.0414746553, 0)
+TextLabel_2.Size = UDim2.new(0, 304, 0, 46)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "(Gui may NOT work on small devices.) Thank you for using my ui."
+TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextWrapped = true
+
+UICorner_4.Parent = TextLabel_2
+
 Tab_Universal.Name = "Tab_Universal"
 Tab_Universal.Parent = Ui
 Tab_Universal.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
@@ -144,8 +162,9 @@ Tab_Universal.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Tab_Universal.BorderSizePixel = 0
 Tab_Universal.Position = UDim2.new(0.151133507, 0, 0.157894731, 0)
 Tab_Universal.Size = UDim2.new(0, 329, 0, 217)
+Tab_Universal.Visible = false
 
-UICorner_4.Parent = Tab_Universal
+UICorner_5.Parent = Tab_Universal
 
 Tabs_2.Name = "Tabs"
 Tabs_2.Parent = Tab_Universal
@@ -157,7 +176,7 @@ Tabs_2.Position = UDim2.new(-0.14284119, 0, -0.169238105, 0)
 Tabs_2.Size = UDim2.new(0, 39, 0, 253)
 Tabs_2.ZIndex = 0
 
-UICorner_5.Parent = Tabs_2
+UICorner_6.Parent = Tabs_2
 
 Universal_2.Name = "Universal"
 Universal_2.Parent = Tabs_2
@@ -199,19 +218,19 @@ Topbar_2.Position = UDim2.new(-0.00163589057, 0, -0.175439999, 0)
 Topbar_2.Size = UDim2.new(0, 329, 0, 30)
 Topbar_2.ZIndex = 2
 
-UICorner_6.Parent = Topbar_2
+UICorner_7.Parent = Topbar_2
 
-TextLabel_2.Parent = Topbar_2
-TextLabel_2.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
-TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel_2.BorderSizePixel = 0
-TextLabel_2.Position = UDim2.new(0.0425531901, 0, 0.233333334, 0)
-TextLabel_2.Size = UDim2.new(0, 314, 0, 15)
-TextLabel_2.Font = Enum.Font.Cartoon
-TextLabel_2.Text = "Basic UI - Universal"
-TextLabel_2.TextColor3 = Color3.fromRGB(192, 192, 192)
-TextLabel_2.TextSize = 25.000
-TextLabel_2.TextXAlignment = Enum.TextXAlignment.Left
+TextLabel_3.Parent = Topbar_2
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
+TextLabel_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.BorderSizePixel = 0
+TextLabel_3.Position = UDim2.new(0.0425531901, 0, 0.233333334, 0)
+TextLabel_3.Size = UDim2.new(0, 314, 0, 15)
+TextLabel_3.Font = Enum.Font.Cartoon
+TextLabel_3.Text = "Basic UI - Universal"
+TextLabel_3.TextColor3 = Color3.fromRGB(192, 192, 192)
+TextLabel_3.TextSize = 25.000
+TextLabel_3.TextXAlignment = Enum.TextXAlignment.Left
 
 _64Speed.Name = "64 Speed"
 _64Speed.Parent = Tab_Universal
@@ -227,7 +246,7 @@ _64Speed.TextScaled = true
 _64Speed.TextSize = 20.000
 _64Speed.TextWrapped = true
 
-UICorner_7.Parent = _64Speed
+UICorner_8.Parent = _64Speed
 
 Tab_Other.Name = "Tab_Other"
 Tab_Other.Parent = Ui
@@ -238,7 +257,7 @@ Tab_Other.Position = UDim2.new(0.151133507, 0, 0.157894731, 0)
 Tab_Other.Size = UDim2.new(0, 329, 0, 217)
 Tab_Other.Visible = false
 
-UICorner_8.Parent = Tab_Other
+UICorner_9.Parent = Tab_Other
 
 Tabs_3.Name = "Tabs"
 Tabs_3.Parent = Tab_Other
@@ -250,7 +269,7 @@ Tabs_3.Position = UDim2.new(-0.14284119, 0, -0.169238105, 0)
 Tabs_3.Size = UDim2.new(0, 39, 0, 253)
 Tabs_3.ZIndex = 0
 
-UICorner_9.Parent = Tabs_3
+UICorner_10.Parent = Tabs_3
 
 Universal_3.Name = "Universal"
 Universal_3.Parent = Tabs_3
@@ -292,19 +311,19 @@ Topbar_3.Position = UDim2.new(-0.00163589057, 0, -0.175439999, 0)
 Topbar_3.Size = UDim2.new(0, 329, 0, 30)
 Topbar_3.ZIndex = 2
 
-UICorner_10.Parent = Topbar_3
+UICorner_11.Parent = Topbar_3
 
-TextLabel_3.Parent = Topbar_3
-TextLabel_3.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
-TextLabel_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel_3.BorderSizePixel = 0
-TextLabel_3.Position = UDim2.new(0.0425531901, 0, 0.233333334, 0)
-TextLabel_3.Size = UDim2.new(0, 314, 0, 15)
-TextLabel_3.Font = Enum.Font.Cartoon
-TextLabel_3.Text = "Basic UI - Other"
-TextLabel_3.TextColor3 = Color3.fromRGB(192, 192, 192)
-TextLabel_3.TextSize = 25.000
-TextLabel_3.TextXAlignment = Enum.TextXAlignment.Left
+TextLabel_4.Parent = Topbar_3
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
+TextLabel_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.BorderSizePixel = 0
+TextLabel_4.Position = UDim2.new(0.0425531901, 0, 0.233333334, 0)
+TextLabel_4.Size = UDim2.new(0, 314, 0, 15)
+TextLabel_4.Font = Enum.Font.Cartoon
+TextLabel_4.Text = "Basic UI - Other"
+TextLabel_4.TextColor3 = Color3.fromRGB(192, 192, 192)
+TextLabel_4.TextSize = 25.000
+TextLabel_4.TextXAlignment = Enum.TextXAlignment.Left
 
 LearnLua.Name = "Learn Lua"
 LearnLua.Parent = Tab_Other
@@ -320,17 +339,15 @@ LearnLua.TextScaled = true
 LearnLua.TextSize = 20.000
 LearnLua.TextWrapped = true
 
-UICorner_11.Parent = LearnLua
-
-UIAspectRatioConstraint.Parent = Ui
-UIAspectRatioConstraint.AspectRatio = 1.492
+UICorner_12.Parent = LearnLua
 
 HideShow.Name = "Hide/Show"
 HideShow.Parent = BasicUI
+HideShow.AnchorPoint = Vector2.new(0.5, 0.5)
 HideShow.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
 HideShow.BorderColor3 = Color3.fromRGB(0, 0, 0)
 HideShow.BorderSizePixel = 0
-HideShow.Position = UDim2.new(0.462396115, 0, -0.00161526352, 0)
+HideShow.Position = UDim2.new(0.499924541, 0, 0.023130931, 0)
 HideShow.Size = UDim2.new(0, 95, 0, 25)
 HideShow.ZIndex = 3
 HideShow.Font = Enum.Font.SourceSans
@@ -339,17 +356,11 @@ HideShow.TextColor3 = Color3.fromRGB(75, 124, 125)
 HideShow.TextSize = 25.000
 HideShow.TextXAlignment = Enum.TextXAlignment.Right
 
-UICorner_12.Parent = HideShow
-
-UIAspectRatioConstraint_2.Parent = HideShow
-UIAspectRatioConstraint_2.AspectRatio = 3.800
-
-UIAspectRatioConstraint_3.Parent = BasicUI
-UIAspectRatioConstraint_3.AspectRatio = 2.212
+UICorner_13.Parent = HideShow
 
 -- Scripts:
 
-local function UVOJEBB_fake_script() -- Universal.Change to Uni 
+local function WKDRNL_fake_script() -- Universal.Change to Uni 
 	local script = Instance.new('LocalScript', Universal)
 
 	local button = script.Parent
@@ -364,8 +375,8 @@ local function UVOJEBB_fake_script() -- Universal.Change to Uni
 		end
 	end)
 end
-coroutine.wrap(UVOJEBB_fake_script)()
-local function USWX_fake_script() -- Other.Change to Oth 
+coroutine.wrap(WKDRNL_fake_script)()
+local function CDKHKTD_fake_script() -- Other.Change to Oth 
 	local script = Instance.new('LocalScript', Other)
 
 	local button = script.Parent
@@ -380,8 +391,8 @@ local function USWX_fake_script() -- Other.Change to Oth
 		end
 	end)
 end
-coroutine.wrap(USWX_fake_script)()
-local function DKVEYD_fake_script() -- Home_2.Change to Hom 
+coroutine.wrap(CDKHKTD_fake_script)()
+local function RULKKR_fake_script() -- Home_2.Change to Hom 
 	local script = Instance.new('LocalScript', Home_2)
 
 	local button = script.Parent
@@ -396,8 +407,8 @@ local function DKVEYD_fake_script() -- Home_2.Change to Hom
 		end
 	end)
 end
-coroutine.wrap(DKVEYD_fake_script)()
-local function RFRMYGU_fake_script() -- Other_2.Change to Oth 
+coroutine.wrap(RULKKR_fake_script)()
+local function UERVZU_fake_script() -- Other_2.Change to Oth 
 	local script = Instance.new('LocalScript', Other_2)
 
 	local button = script.Parent
@@ -412,9 +423,9 @@ local function RFRMYGU_fake_script() -- Other_2.Change to Oth
 		end
 	end)
 end
-coroutine.wrap(RFRMYGU_fake_script)()
+coroutine.wrap(UERVZU_fake_script)()
 -- _64Speed.Script is disabled.
-local function MJJT_fake_script() -- _64Speed.LocalScript 
+local function HIUCFJ_fake_script() -- _64Speed.LocalScript 
 	local script = Instance.new('LocalScript', _64Speed)
 
 	local Players = game:GetService("Players")
@@ -426,8 +437,8 @@ local function MJJT_fake_script() -- _64Speed.LocalScript
 		humanoid.WalkSpeed = 64
 	end)
 end
-coroutine.wrap(MJJT_fake_script)()
-local function KJIDP_fake_script() -- Universal_3.Change to Uni 
+coroutine.wrap(HIUCFJ_fake_script)()
+local function OURIQ_fake_script() -- Universal_3.Change to Uni 
 	local script = Instance.new('LocalScript', Universal_3)
 
 	local button = script.Parent
@@ -442,8 +453,8 @@ local function KJIDP_fake_script() -- Universal_3.Change to Uni
 		end
 	end)
 end
-coroutine.wrap(KJIDP_fake_script)()
-local function CTYY_fake_script() -- Home_3.Change to Hom 
+coroutine.wrap(OURIQ_fake_script)()
+local function AORKJZ_fake_script() -- Home_3.Change to Hom 
 	local script = Instance.new('LocalScript', Home_3)
 
 	local button = script.Parent
@@ -458,9 +469,9 @@ local function CTYY_fake_script() -- Home_3.Change to Hom
 		end
 	end)
 end
-coroutine.wrap(CTYY_fake_script)()
+coroutine.wrap(AORKJZ_fake_script)()
 -- LearnLua.Script is disabled.
-local function GEIFLKS_fake_script() -- LearnLua.LocalScript 
+local function VSEL_fake_script() -- LearnLua.LocalScript 
 	local script = Instance.new('LocalScript', LearnLua)
 
 	local Players = game:GetService("Players")
@@ -472,8 +483,8 @@ local function GEIFLKS_fake_script() -- LearnLua.LocalScript
 		humanoid.WalkSpeed = 64
 	end)
 end
-coroutine.wrap(GEIFLKS_fake_script)()
-local function KWIPV_fake_script() -- HideShow.Hide or Show Script 
+coroutine.wrap(VSEL_fake_script)()
+local function LJIRIT_fake_script() -- HideShow.Hide or Show Script 
 	local script = Instance.new('LocalScript', HideShow)
 
 	local button = script.Parent
@@ -489,4 +500,4 @@ local function KWIPV_fake_script() -- HideShow.Hide or Show Script
 	end)
 	
 end
-coroutine.wrap(KWIPV_fake_script)()
+coroutine.wrap(LJIRIT_fake_script)()
