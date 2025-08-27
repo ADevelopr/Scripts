25f788fb1233e681ed227d50f4f004778c1ecf82
TheDumb <fofoangel858@gmail.com>
2025-08-27 17:07:24 -0600

    Update GUI Project.lua

--- GUI Project.lua
+++ GUI Project.lua
@@ -1,6 +1,27 @@
--- Define the module table directly in the script
-local igiveup = {
-    test = function()
-        print("Hello world!")
-    end
+
+
+
+
+local basiccal = {
+	
 }
+
+function basiccal.add(num1 , num2)
+	local result = num1 + num2
+	return result
+end
+
+function basiccal.sub(num1 , num2)
+	local result = num1 - num2
+	return result
+end
+
+function basiccal.mul(num1 , num2)
+	local result = num1 * num2
+	return result
+end
+
+function basiccal.div(num1 , num2)
+	local result = num1 / num2
+	return result
+end
