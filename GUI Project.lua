d662019f3a2f11e7533aa02bb9810dfb9efea910
TheDumb <fofoangel858@gmail.com>
2025-08-27 20:02:57 -0500

    Update GUI Project.lua

--- GUI Project.lua
+++ GUI Project.lua
@@ -1,27 +1,3 @@
-
-
-
-
-local basiccal = {
-	
-}
-
-function basiccal.add(num1 , num2)
-	local result = num1 + num2
-	return result
-end
-
-function basiccal.sub(num1 , num2)
-	local result = num1 - num2
-	return result
-end
-
-function basiccal.mul(num1 , num2)
-	local result = num1 * num2
-	return result
-end
-
-function basiccal.div(num1 , num2)
-	local result = num1 / num2
-	return result
+local function myfuction()
+    print('hello world')
 end
