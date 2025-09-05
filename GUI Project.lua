b3a1bdc9afa7b6682a482e8a6659059e66de08e9
TheDumb <fofoangel858@gmail.com>
2025-09-05 16:12:35 -0600

    Update GUI Project.lua

--- GUI Project.lua
+++ GUI Project.lua
@@ -1,7 +1,15 @@
-local function cal(num1, num2)
-	local res = num1 + num2
+	frame = function()
+		local ScreenGui = Instance.new('ScreenGui')
+		ScreenGui.Parent = game.Players.LocalPlayer.PlayerGui
+		ScreenGui.IgnoreGuiInset = true
+		ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+		
+		local Frame = Instance.new('Frame')
+		Frame.Parent = ScreenGui
+		Frame.BorderSizePixel = 0
+		Frame.BorderColor3 = Color3.fromRGB(255, 255, 255)
+		Frame.Size = UDim2.new(0, 100, 0, 100)
+		Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	end
 
-	return res
-end
-
-return cal
+return frame
